b3d8678e254e6efb9b226317580f26a160a23ab4
Eric Horacek <horacek.eric@gmail.com>
2013-03-15 19:00:20 -0600

    Adds a header that can be imported into the prefix

---
+++ MSSocialKit/MSSocialKit.h
@@ -0,0 +1,12 @@
+//
+//  MSSocialKit.h
+//  Pods
+//
+//  Created by Eric Horacek on 3/14/13.
+//
+//
+
+#import "MSSocialKitManager.h"
+#import "MSTweetsViewController.h"
+#import "MSInstagramPhotoViewController.h"
+#import "MSSocialCell.h"
